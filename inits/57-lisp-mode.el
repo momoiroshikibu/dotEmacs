3cb1c4c42b2e187d477e97e9e97685d9c154d278
momoiroshikibu <momoiroshikibu@gmail.com>
2016-09-11 22:22:48 +0900

    Update settings for lisp-mode

--- inits/57-lisp-mode.el
+++ inits/57-lisp-mode.el
@@ -1,9 +1,3 @@
-
-
-
-;; (when (require 'rainbow-delimiter-mode))
 (when (require 'rainbow-delimiters)
   (add-hook 'lisp-mode-hook 'rainbow-delimiters-mode)
   (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode))
-
-
