5be80fe2861ed15b2ad39bbdbc9fc78b555e0ba5
momoiroshikibu <momoiroshikibu@gmail.com>
2016-09-22 08:45:30 +0900

    roswell selime-helper

--- inits/57-lisp-mode.el
+++ inits/57-lisp-mode.el
@@ -1,3 +1,18 @@
 (when (require 'rainbow-delimiters)
   (add-hook 'lisp-mode-hook 'rainbow-delimiters-mode)
   (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode))
+
+
+(load (expand-file-name "~/.roswell/lisp/quicklisp/slime-helper.el"))
+;; Replace "sbcl" with the path to your implementation
+(setq inferior-lisp-program "sbcl")
+
+
+
+
+
+
+
+
+
+
