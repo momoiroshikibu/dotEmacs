642d8b37e4ad15ecb76c92dc566ec7960248f0d2
momoiroshikibu <momoiroshikibu@gmail.com>
2017-02-25 17:57:42 +0900

    markdown css

--- inits/60-markdown.el
+++ inits/60-markdown.el
@@ -1,4 +1,3 @@
-;; npm install -g marked
 (when (require 'markdown-mode)
   (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
   (add-to-list 'auto-mode-alist '("\\.mdown$" . markdown-mode))
@@ -7,5 +6,4 @@
             (lambda ()
               (visual-line-mode t)
               (writegood-mode t)))
-  (setq markdown-command "pandoc --smart -f markdown -t html"))
-
+  (setq markdown-command "pandoc --smart -f markdown_github -t html5 -c ~/.emacs.d/markdown.css"))
